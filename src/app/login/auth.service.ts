ec64ce1a1230001d52a90d891f15330619187214
Thomas Boruszewski <t.boruszewski@groupecerise.fr>
2017-07-21 17:57:27 +0200

    Login tests

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -1,42 +1,65 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import { Params } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class AuthService {
-  private headers = new Headers({'Accept': 'application/vnd.twitchtv.v5+json'});
   private clientId = '9yj5uqj5fywartq4hewxwkx7dzlyzo';
   private baseUrl = 'https://api.twitch.tv/kraken/oauth2/authorize';
-  private scope = 'user_read+channel_read';
+  private scope = 'channel_read';
   private redirectUri = 'http://localhost:4200/login';
+  private state: string;
+  private nonce: string;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+      this.state = this.random();
+      this.nonce = this.random();
+  }
 
   isAuth () {
-    return localStorage.getItem('token');
+    return localStorage.getItem('hash');
   }
 
   authUrl(): string {
     const uri = encodeURIComponent(this.redirectUri);
-    const state = this.random();
-    const nonce = this.random();
 
-    return `${this.baseUrl}?response_type=code&client_id=${this.clientId}&redirect_uri=${uri}&scope=${this.scope}&state=${state}&nonce=${nonce}`;
+    return `${this.baseUrl}?response_type=code&client_id=${this.clientId}&redirect_uri=${uri}&scope=${this.scope}&state=${this.state}&nonce=${this.nonce}`;
   }
 
-  login(code: string): Observable<any> {
+  login(params: Params): Observable<any> {
     // http://localhost:4200/login?code=fzm9arkhdvec80ns1f7kkx2eexyjux&scope=user_read&state=123456
     // http://localhost:4200/login?error=redirect_mismatch&error_description=Parameter%2Bredirect_uri%2Bdoes%2Bnot%2Bmatch%2Bregistered%2BURI&state=123456
 
-    const url = `http://localhost:8000/auth?code=${code}`;
+    if (typeof params.error !== 'undefined') {
+        return Observable.throw(params.error_description);
+    }
 
+    if (typeof params.code === 'undefined') {
+        return Observable.throw('Invalid code');
+    }
+
+    if (typeof params.state === 'undefined' || params.state !== this.state) {
+        //return Observable.throw('Invalid state');
+    }
+
+    const url = `http://localhost:8000/auth?code=${params.code}`;
+
+    console.log(url);
+    return;
+/*
     return this.http.get(url)
-        .map((response: Response) => response)
-        .catch((error: any) => Observable.throw(error));
+        .map((response: Response) => {
+            console.log(response.json());
+            localStorage.setItem('hash', response.json().hash);
+            return response.json();
+        })
+        .catch((error: any) => Observable.throw(error));*/
   }
 
   random() {
@@ -44,6 +67,6 @@ export class AuthService {
   }
 
   logout() {
-    localStorage.removeItem('token');
+    localStorage.removeItem('hash');
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,39 +5,33 @@ import { AuthService } from './auth.service';
 import { AlertService } from '../alert/alert.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  constructor(
-      private authService: AuthService,
-      private alertService: AlertService,
-      private activatedRoute: ActivatedRoute,
-      private router: Router
-  ) { }
+    constructor(
+        private authService: AuthService,
+        private alertService: AlertService,
+        private activatedRoute: ActivatedRoute,
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-    this.activatedRoute.queryParams.subscribe((params: Params) => {
-      console.log(params);
-
-      if (typeof params.code !== 'undefined') {
-        this.authService.login(params.code).subscribe(data => {
-          this.alertService.success(data);
-        },
-        error => {
-          this.alertService.error(error);
+    ngOnInit() {
+        this.activatedRoute.queryParams.subscribe((params: Params) => {
+            if (typeof params.code !== 'undefined') {
+                this.authService.login(params).subscribe(data => {
+                    this.alertService.success(data);
+                },
+                error => {
+                    this.alertService.error(error);
+                });
+            }
         });
-      }
+    }
 
-      if (typeof params.error !== 'undefined') {
-        this.alertService.error(params.error_description);
-      }
-    });
-  }
-
-  login() {
-    // console.log(this.authService.twitchUrl());
-    window.location.href = this.authService.authUrl();
-  }
+    login() {
+        // console.log(this.authService.twitchUrl());
+        window.location.href = this.authService.authUrl();
+    }
 }
